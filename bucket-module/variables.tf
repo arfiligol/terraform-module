a768e46f0272aaaacddc0cde4fff58b2b90e495a
arfiligol <96962407+arfiligol@users.noreply.github.com>
2023-07-04 15:02:00 +0800

    Create variables.tf

---
+++ bucket-module/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "The name of the bucket."
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the bucket."
+  type        = string
+}
