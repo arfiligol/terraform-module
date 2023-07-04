5127a50c999a9ae57e61851bd4cc1f9af2e96c80
邱羿理 <arfiligol@qiuyilideMacBook-Pro.local>
2023-07-04 17:10:01 +0800

    clean outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  value       = google_storage_bucket.my_bucket.name
-  description = "The name of the created bucket"
-}
-
-output "bucket_url" {
-  value       = "gs://${google_storage_bucket.my_bucket.name}"
-  description = "The URL of the created bucket"
-}
