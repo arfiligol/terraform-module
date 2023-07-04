0878dc4e2da43c458eaf3abecf1b266a8ba01479
arfiligol <96962407+arfiligol@users.noreply.github.com>
2023-07-04 16:37:38 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
-module "bucket" {
-  source = "./bucket-module"
+provider "google" {
+  project = "astute-city-391210"
+  region  = "asia-east1"
 
-  bucket_name = var.bucket_name
-  location    = var.location
+
 }
+
+resource "google_storage_bucket" "default" {
+  name          = "ili-terraform-created-bucket"
+  force_destroy = false
+  location      = "asia-east1"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
+
