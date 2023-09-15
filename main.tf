9cc13a171f43640b4f7098595cdd8720af9b6332
arfiligol <arfiligol@gmail.com>
2023-09-15 21:06:48 +0800

    alter main let it comment resource first

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "my_bucket" {
-  name     = var.gcs_demo_bucket_name # 這裡請確保使用一個獨特的名稱
-  location = "asia-east1"           # 這是 region，GCP 的 bucket 不需要特定的 zone
-  storage_class = "STANDARD"         # 這是 Class Type，可以是 STANDARD, NEARLINE, COLDLINE, ARCHIVE
+# resource "google_storage_bucket" "my_bucket" {
+#   name     = var.gcs_demo_bucket_name # 這裡請確保使用一個獨特的名稱
+#   location = "asia-east1"           # 這是 region，GCP 的 bucket 不需要特定的 zone
+#   storage_class = "STANDARD"         # 這是 Class Type，可以是 STANDARD, NEARLINE, COLDLINE, ARCHIVE
 
-  # 強制使用 IAM 進行存取控制
-  uniform_bucket_level_access = false
-}
+#   # 強制使用 IAM 進行存取控制
+#   uniform_bucket_level_access = false
+# }
